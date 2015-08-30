596708d6cab6eeda86552e3aeacf20412571e0ad
bsv-DongNguyen <dongnguyenduc@Dongs-MacBook-Pro.local>
2015-08-30 17:09:33 +0700

    Initial Commit

---
+++ IAmRich/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IAmRich
+//
+//  Created by Dong Nguyen Duc on 8/30/15.
+//  Copyright (c) 2015 Dong Nguyen Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IAmRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAmRich
+//
+//  Created by Dong Nguyen Duc on 8/30/15.
+//  Copyright (c) 2015 Dong Nguyen Duc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
