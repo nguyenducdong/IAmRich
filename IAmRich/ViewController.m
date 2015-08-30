bc7bae0f85e56b0179f69b1f362ce5bb78ae4187
bsv-DongNguyen <dongnguyenduc@Dongs-MacBook-Pro.local>
2015-08-30 18:53:09 +0700

    Add bài tập zigzag

--- IAmRich/ViewController.m
+++ IAmRich/ViewController.m
@@ -23,6 +23,9 @@
     self.ruby.alpha = 0;
     self.iamrich.alpha = 0;
             [self.verticalLabel_Constrant setConstant:-21.0f];
+    
+    [self zigzagwithCol:21 Row:5];
+
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -30,10 +33,10 @@
     [super viewWillAppear:animated];
     CGRect frameRuby = self.ruby.frame;
     float pointY = frameRuby.origin.y + frameRuby.size.height + 8;
-    NSLog(@"ruby's alpha = %1.0f", self.ruby.alpha);
+//    NSLog(@"ruby's alpha = %1.0f", self.ruby.alpha);
     [UIView animateWithDuration:4 animations:^{
         self.ruby.alpha = 1;
-        NSLog(@"ruby's alpha = %1.0f", self.ruby.alpha);
+//        NSLog(@"ruby's alpha = %1.0f", self.ruby.alpha);
     } completion:^(BOOL finished) {
         [UIView animateWithDuration:3 animations:^{
             [self.verticalLabel_Constrant setConstant:pointY];
@@ -42,5 +45,33 @@
         } completion:nil];
     }];
 }
+
+- (void)zigzagwithCol:(int)nCol Row:(int)nRow{
+    nRow--;
+    NSMutableArray *ayy = [NSMutableArray array];
+    int length = 2 * nRow;
+    for (int col = 0; col < nCol; col++) {
+        int index = col % length;
+        [ayy addObject:@(index)];
+    }
+//    for (int row = 0; row <= nRow; row++) {
+//        for (int col = 0; col < nCol; col++) {
+//            if ([ayy[col] intValue] == row || [ayy[col] intValue] == length - row) printf("*");
+//            else printf("-");
+//        }
+//        printf("\n");
+//    }
+    
+    for (int row = 0; row <= nRow; row++) {
+        for (int col = nCol - 1; col >= 0; col--) {
+            if ([ayy[col] intValue] == row || [ayy[col] intValue] == length - row) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    
+    
+}
+
 
 @end
